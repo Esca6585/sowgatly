dbcbe55d7b7370f1fec9a97d43877b491581a642
Esen Meredow <esca656585@gmail.com>
2024-08-22 19:46:15 +0500

    Update from Artisan command - inside data updated

--- config/auth.php
+++ config/auth.php
@@ -41,11 +41,6 @@ return [
             'provider' => 'users',
         ],
 
-        'seller' => [
-            'driver' => 'session',
-            'provider' => 'sellers',
-        ],
-
         'admin' => [
             'driver' => 'session',
             'provider' => 'admins',
@@ -81,11 +76,6 @@ return [
             'model' => App\Models\User::class,
         ],
 
-        'sellers' => [
-            'driver' => 'eloquent',
-            'model' => App\Models\Seller::class,
-        ],
-
         'admins' => [
             'driver' => 'eloquent',
             'model' => App\Models\Admin::class,
@@ -109,13 +99,6 @@ return [
 
     'passwords' => [
         'users' => [
-            'provider' => 'users',
-            'table' => 'password_resets',
-            'expire' => 60,
-            'throttle' => 60,
-        ],
-
-        'sellers' => [
             'provider' => 'users',
             'table' => 'password_resets',
             'expire' => 60,
