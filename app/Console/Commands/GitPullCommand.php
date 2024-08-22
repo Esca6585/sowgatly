f6f965f455255d355869063f3641e8f32d38daa7
Esen Meredow <esca656585@gmail.com>
2024-08-22 20:20:12 +0500

    Update from Artisan command - inside data updated

--- app/Console/Commands/GitPullCommand.php
+++ app/Console/Commands/GitPullCommand.php
@@ -11,6 +11,9 @@ class GitPullCommand extends Command
 
     public function handle()
     {
+        $this->info('Generating Swagger documentation...');
+        $this->call('swagger:generate');
+
         $this->info('Resetting local changes...');
         exec('git reset --hard', $resetOutput, $resetReturnCode);
 
@@ -38,8 +41,5 @@ class GitPullCommand extends Command
                 $this->error($line);
             }
         }
-
-        $this->info('Generating Swagger documentation...');
-        $this->call('swagger:generate');
     }
 }
