812381a1d798ab948760c8674d0bee2c686b7f80
Esen Meredow <esca656585@gmail.com>
2024-08-22 17:47:06 +0500

    Update from Artisan command - inside data updated

--- database/migrations/2024_08_22_172224_create_brands_table.php
+++ database/migrations/2024_08_22_172224_create_brands_table.php
@@ -4,28 +4,48 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateProductsTable extends Migration
 {
     /**
      * Run the migrations.
+     *
+     * @return void
      */
-    public function up(): void
+    public function up()
     {
-        Schema::create('brands', function (Blueprint $table) {
+        Schema::create('products', function (Blueprint $table) {
             $table->id();
+            
             $table->string('name');
             $table->text('description')->nullable();
-            $table->string('logo')->nullable();
-            $table->boolean('status')->default(true);
+            
+            $table->decimal('price', 10, 2);
+            $table->decimal('discount', 5, 2)->default(0);
+            
+            $table->string('attributes')->nullable();
+            
+            $table->string('code')->unique();
+            
+            $table->unsignedBigInteger('category_id');
+            $table->unsignedBigInteger('shop_id');
+            $table->unsignedBigInteger('brand_id'); // Added brand_id column
+
+            $table->boolean('status')->default(0);
             $table->timestamps();
+
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+            $table->foreign('shop_id')->references('id')->on('shops')->onDelete('cascade');
+            $table->foreign('brand_id')->references('id')->on('brands')->onDelete('cascade'); // Added foreign key constraint
         });
     }
 
     /**
      * Reverse the migrations.
+     *
+     * @return void
      */
-    public function down(): void
+    public function down()
     {
-        Schema::dropIfExists('brands');
+        Schema::dropIfExists('products');
     }
-};
+}
