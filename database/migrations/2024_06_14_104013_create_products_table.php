9bc671e5e55302ea1b3ac04502f740a9034d0765
Esen Meredow <esca656585@gmail.com>
2024-08-22 18:46:43 +0500

    Update from Artisan command - inside data updated

--- database/migrations/2024_06_14_104013_create_products_table.php
+++ database/migrations/2024_06_14_104013_create_products_table.php
@@ -4,20 +4,19 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateProductsTable extends Migration
+return new class extends Migration
 {
     public function up()
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            
             $table->string('name');
             $table->decimal('price', 10, 2);
             $table->integer('discount')->nullable();
             $table->text('description');
             $table->string('gender')->nullable()->comment('Men, Women, Children bolup bilya');
-            $table->json('sizes')->nullable()->comment('42, 43,...,50 yaly olcegler array gornushunde saklanmaly');
-            $table->json('separated_sizes')->nullable()->comment('S, M, L yaly olcegler array gornushunde saklanmaly');
+            $table->text('sizes')->nullable()->comment('42, 43,...,50 yaly olcegler array gornushunde saklanmaly');
+            $table->text('separated_sizes')->nullable()->comment('S, M, L yaly olcegler array gornushunde saklanmaly');
             $table->string('color')->nullable();
             $table->text('manufacturer')->nullable()->comment('Cykarylan yurdy');
             $table->double('width')->nullable();
@@ -27,16 +26,10 @@ class CreateProductsTable extends Migration
             $table->integer('min_order')->nullable();
             $table->boolean('seller_status')->comment('Bu dukancy tarapyndan berilmeli status');
             $table->boolean('status')->comment('Bu administrator tarapyndan berilmeli status');
-
-            $table->unsignedBigInteger('shop_id');
-            $table->unsignedBigInteger('brand_id');
-            $table->unsignedBigInteger('category_id');
-            
+            $table->foreignId('shop_id')->constrained();
+            $table->foreignId('brand_id')->constrained();
+            $table->foreignId('category_id')->constrained();
             $table->timestamps();
-
-            $table->foreign('shop_id')->references('id')->on('shops')->onDelete('cascade');
-            $table->foreign('brand_id')->references('id')->on('brands')->onDelete('cascade');
-            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
         });
     }
 
@@ -44,4 +37,4 @@ class CreateProductsTable extends Migration
     {
         Schema::dropIfExists('products');
     }
-}
+};
