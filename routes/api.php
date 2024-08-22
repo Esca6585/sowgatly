f788301473ff2cd5b73d41c574fc971c902e7dbb
Esen Meredow <esca656585@gmail.com>
2024-08-22 17:28:35 +0500

    Update from Artisan command - inside data updated

--- routes/api.php
+++ routes/api.php
@@ -15,25 +15,39 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::controller(App\Http\Controllers\API\AuthOtpController::class)->group(function(){
+    // OTP Generate route
     Route::post('otp/generate', 'generate');
+
+    // Login with otp route
     Route::post('login', 'loginWithOtp');
+
+    // Register with otp route
     Route::post('register', 'registerWithOtp');
+
+    // logout route
     Route::post('logout', 'logout')->middleware(['auth:sanctum', 'check.token']);
 });
 
 Route::middleware(['auth:sanctum', 'check.token'])->group(function () {
+    // Products routes
     Route::apiResource('products', App\Http\Controllers\API\ProductController::class);
     Route::get('product/search', [App\Http\Controllers\API\ProductController::class , 'search']);
     Route::get('product/category/{category_id}', [App\Http\Controllers\API\ProductController::class , 'getByCategory']);
     
+    // Categories routes
     Route::apiResource('categories', App\Http\Controllers\API\CategoryController::class);
     Route::get('/categories/{id}/subcategories', [App\Http\Controllers\API\CategoryController::class, 'getSubcategories']);
 
+    // Users routes
     Route::apiResource('users', App\Http\Controllers\API\UserController::class);
 
+    // Shops routes
     Route::apiResource('shops', App\Http\Controllers\API\ShopController::class);
 
-    // Cart routes
+    // Brand routes
+    Route::apiResource('brands', BrandController::class);
+
+    // Carts routes
     Route::apiResource('carts', App\Http\Controllers\API\CartController::class);
     Route::post('cart/add', [App\Http\Controllers\API\CartController::class, 'addToCart']);
     Route::get('cart', [App\Http\Controllers\API\CartController::class, 'getCart']);
