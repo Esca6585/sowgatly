575014aa299021db2bd0df6615abddeb06aa617f
Esen Meredow <esca656585@gmail.com>
2024-08-22 20:19:52 +0500

    Update from Artisan command - inside data updated

--- app/Models/Composition.php
+++ app/Models/Composition.php
@@ -9,11 +9,29 @@ class Composition extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['name'];
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+    ];
 
+    /**
+     * Get the products that use this composition.
+     */
     public function products()
     {
         return $this->belongsToMany(Product::class, 'product_compositions')
                     ->withPivot('qty', 'qty_type');
+    }
+
+    /**
+     * Get the product compositions for this composition.
+     */
+    public function productCompositions()
+    {
+        return $this->hasMany(ProductComposition::class);
     }
 }
