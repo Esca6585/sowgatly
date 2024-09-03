fe86ec15bf3d5143bb397a8a1d2b6ecdf8ff6c14
Esen Meredow <esca656585@gmail.com>
2024-09-03 15:07:34 +0500

    Update from Artisan command - inside data updated

--- app/Http/Resources/CompositionResource.php
+++ app/Http/Resources/CompositionResource.php
@@ -5,6 +5,17 @@ namespace App\Http\Resources;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
+/**
+ * @OA\Schema(
+ *     schema="CompositionResource",
+ *     title="Composition Resource",
+ *     description="Composition resource",
+ *     @OA\Property(property="id", type="integer"),
+ *     @OA\Property(property="name", type="string"),
+ *     @OA\Property(property="created_at", type="string", format="date-time"),
+ *     @OA\Property(property="updated_at", type="string", format="date-time")
+ * )
+ */
 class CompositionResource extends JsonResource
 {
     /**
